e21c8f05a0c91a763cc09a79dd8d70db51de8fef
ThatianaMattos <thatianamattos25@gmail.com>
2025-11-13 16:11:42 -0300

    feat: implementar models e interface do e-commerce

---
+++ src/model/TesteProduto.ts
@@ -0,0 +1,13 @@
+import { ProdutoDigital } from "./ProdutoDigital";
+
+const prod = new ProdutoDigital(
+  100,
+  "CSS Completo",
+  "Desenvolvimento Web",
+  59.9,
+  "Iniciante",
+  20,
+  true
+);
+
+prod.visualizar();
